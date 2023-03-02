e962d8b00b36ab3fa0b4341d8143f0e93c187f33
summer_fi2021 <summer_fi2021@163.com>
2023-03-02 23:37:28 +0800

    feat: va

---
+++ go.mod
@@ -0,0 +1,3 @@
+module jj/v2
+
+go 1.17
--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"jj/v2/user"
+)
 
 func main() {
-	fmt.Println("hello world")
+	s := user.Hello()
+	fmt.Printf("s: %v\n", s)
+
+	// var name string
+	// var age int
+	// var bol bool
+
+	// var (
+	// 	name string
+	// 	age  int
+	// 	bol  bool
+	// )
+
+	// var (
+	// 	name string = "tom"
+	// 	age  int    = 20
+	// 	bol  bool   = true
+	// )
+
+	// var name, age, bol = "tom", 20, true
+
+	name := "tom"
+	age := 20
+	bol := true
+
+	fmt.Printf("name: %v\n", name)
+	fmt.Printf("age: %v\n", age)
+	fmt.Printf("bol: %v\n", bol)
 }
